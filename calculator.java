d14180e4c881d5e8a74bb9e8c01efafcedb0ab39
sanam abbasi <sanam.abbasi96@yahoo.com>
2019-04-20 20:36:38 +0500

    added switch case for substraction

--- calculator.java
+++ calculator.java
@@ -12,11 +12,19 @@ public class Decimal {
  
         System.out.println("Enter second number");
         n2 = scannerObject. nextDouble();
-Scanner op = new Scanner(System.in);
+
+		Scanner op = new Scanner(System.in);
+        
         System.out.println("Enter your operation");
         operation = op.next();
-switch (operation)  {
+
+	switch (operation)  {
         case "+":
             System.out.println("Your answer is " + (n1 + n2));
             break;
+		
+		case "-":
+            System.out.println("Your answer is " + (n1 - n2));
+            break;
+
 
