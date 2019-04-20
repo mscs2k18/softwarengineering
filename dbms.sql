dcbb61b8bb9979b6d4badcd9adc7be5ce280b1d6
sanam abbasi <sanam.abbasi96@yahoo.com>
2019-04-20 21:20:08 +0500

    sql file added

---
+++ dbms.sql
@@ -0,0 +1,3 @@
+create database central library;
+use central library;
+create table books genres(bookname varchar(30),authorname varchar (30), bookgenre varchar(30),bookid int(30) NOT NULL PRIMARY KEY, numofcopies int(30));
