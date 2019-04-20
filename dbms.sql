8fb5b49f90aab4ac6e1a3ed5f2a9f42898a1af3c
sanam abbasi <sanam.abbasi96@yahoo.com>
2019-04-20 21:37:44 +0500

    sql code modified

--- dbms.sql
+++ dbms.sql
@@ -1,3 +1,7 @@
 create database central library;
 use central library;
 create table books genres(bookname varchar(30),authorname varchar (30), bookgenre varchar(30),bookid int(30) NOT NULL PRIMARY KEY, numofcopies int(30));
+values ('introduction to computer', 'peter norton', 'computer science', 6453, 67 );
+
+create table users (username varchar(30), usertype (varchar), deptartment(30), rollnumber int(30), empid int(30), userid int(30));
+values ('sanam','student','imcs',27,'',6788);
