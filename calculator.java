328eccc7f973a805adb77ac1205b8f12d223c410
sanam abbasi <sanam.abbasi96@yahoo.com>
2019-04-20 14:28:52 +0500

    code added

--- calculator.java
+++ calculator.java
@@ -4,3 +4,6 @@ public class Decimal {
    
     public static void main(String[] args) {
         double n1, n2;
+        String operation;
+        Scanner scannerObject = new Scanner(System.in);
+
